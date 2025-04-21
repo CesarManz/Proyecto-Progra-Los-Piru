713a9dd698e1fc3fbdaf8f34be8f41903e0eeb04
Cesar Manzano <cmanzano23@alumnos.utalca.cl>
2025-04-20 21:46:07 -0400

    prueba no replicas

---
+++ Proyecto-Progra-Los-Piru-master/Proyecto-Progra-Los-Piru-master/ecosmart-backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("EcoSmart backend funcionando 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+/*a la calama*/
+//a
