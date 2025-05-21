1b0c3ee8aefdcbb05e17e11a28860e111b1f308a
Cesar Manzano <cmanzano23@alumnos.utalca.cl>
2025-05-21 18:46:42 -0400

    puebas.env

--- Proyecto-Progra-Los-Piru-master/ecosmart-backend/Rutas/Sensores.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSensorData } = require('../Controllers/sensorController');
-
-router.get('/sensores', getSensorData);
-
-module.exports = router;
--- Proyecto-Progra-Los-Piru-master/ecosmart-backend/Rutas/rutaClima.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { ObtenerClima, ObtenerHistorialClima } = require('../Controllers/conexionClima');
-
-router.get('/clima/:ciudad', ObtenerClima);
-router.get('/clima', ObtenerHistorialClima);
-
-module.exports = router;
