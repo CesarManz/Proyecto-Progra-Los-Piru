93a5ffec09980a7572fa28827fb717927859e94a
xAshura3x <osvaldo.antonio.marchant.gonzalez@gmail.com>
2025-06-08 20:35:47 -0400

    Termine todo el backend de la parte de tareas (falta arreglar estilos)

--- Proyecto-Progra-Los-Piru-master/ecosmart-backend/Rutas/tareas.js
+++ Proyecto-Progra-Los-Piru-master/ecosmart-backend/Rutas/tareas.js
@@ -2,6 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Tarea = require('../Modelos/Tarea');
 
+router.get('/revision', async (req, res) => {
+    try {
+        const tareasRevision = await Tarea.find({ estado: 'en revision' });
+        res.json(tareasRevision);
+    } catch (err) {
+        console.error('❌ Error al obtener tareas en revisión:', err.message);
+        res.status(500).json({ mensaje: 'Error al obtener tareas' });
+    }
+});
+
+router.put('/:id/estado', async (req, res) => {
+    const { id } = req.params;
+    const { nuevoEstado } = req.body;
+
+    try {
+        const tarea = await Tarea.findByIdAndUpdate(id, { estado: nuevoEstado }, { new: true });
+        if (!tarea) {
+            return res.status(404).json({ mensaje: 'Tarea no encontrada' });
+        }
+        res.json(tarea);
+    } catch (err) {
+        console.error('❌ Error al actualizar tarea:', err.message);
+        res.status(500).json({ mensaje: 'Error al actualizar tarea' });
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const { asignacion, titulo, descripcion } = req.body;
@@ -18,6 +44,17 @@ router.post('/', async (req, res) => {
     } catch (err) {
         console.error('❌ Error al guardar tarea:', err.message);
         res.status(500).json({ mensaje: 'Error al guardar tarea' });
+    }
+});
+
+router.get('/asignadas/:correo', async (req, res) => {
+    try {
+        const { correo } = req.params;
+        const tareas = await Tarea.find({ asignacion: correo });
+        res.json(tareas);
+    } catch (err) {
+        console.error('❌ Error al obtener tareas:', err.message);
+        res.status(500).json({ mensaje: 'Error al obtener tareas' });
     }
 });
 
